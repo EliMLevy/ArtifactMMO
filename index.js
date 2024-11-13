f570699638075a042a54a8d73086686c04988d89
Eliyahu Levy <eliyahu.levy21@gmail.com>
2024-11-12 20:50:01 -0500

    Express server to accept new actions for characters

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import Character from "./Character.js";
+import express from 'express';
 
 export const BASE_URL = `https://api.artifactsmmo.com/my/`;
 export const API_TOKEN = "";
@@ -12,36 +13,60 @@ export const miningWorkshop = { x: 1, y: 5 };
 export const yellowSlime = { x: 1, y: -2 };
 export const gudgeonFishing = {x: 4, y: 2}
 
-// goFishing()
-// mineCopper()
-// collectWood()
-// fightLoop();
+// Initialize express
+const app = express();
+app.use(express.json());
 
+// Store character instances in a map for easy access
+const characters = new Map();
+
+// Create and start character instances
 const Bobby = new Character("Bobby");
-// Bobby.addToActionQueue({ state: "move", ...yellowSlime });
-// for(let i = 0; i < 100; i++) {
-//     Bobby.addToActionQueue({ state: "attack" });
-//     Bobby.addToActionQueue({ state: "rest" });
-// }
-Bobby.allActionLoop();
+characters.set("Bobby", Bobby);
 Bobby.actionLoop();
 
 const Stuart = new Character("Stuart");
-Stuart.allActionLoop()
+characters.set("Stuart", Stuart);
 Stuart.actionLoop();
 
-
-
 const George = new Character("George");
-George.allActionLoop()
+characters.set("George", George);
 George.actionLoop();
 
-
 const Tim = new Character("Tim");
-Tim.allActionLoop();
+characters.set("Tim", Tim);
 Tim.actionLoop();
 
-
 const Joe = new Character("Joe");
-Joe.allActionLoop();
+characters.set("Joe", Joe);
 Joe.actionLoop();
+
+// POST endpoint for adding actions to character queues
+app.post('/action', (req, res) => {
+    const { characterName, action, ...actionParams } = req.body;
+    
+    // Validate required fields
+    if (!characterName || !action) {
+        return res.status(400).json({ error: 'Character name and action are required' });
+    }
+
+    // Find character
+    const character = characters.get(characterName);
+    if (!character) {
+        return res.status(404).json({ error: 'Character not found' });
+    }
+
+    // Create action object based on the action type
+    const actionObject = { state: action, ...actionParams };
+
+    // Add action to character's queue
+    character.addToActionQueue(actionObject);
+
+    res.json({ message: `Action ${action} added to ${characterName}'s queue` });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
