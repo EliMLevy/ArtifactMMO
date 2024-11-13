a218df07e04aaddb1e9b619a613f130f20b63a41
Eliyahu Levy <eliyahu.levy21@gmail.com>
2024-11-12 20:49:25 -0500

    Added get resource and getmap base actions

--- BaseActions.js
+++ BaseActions.js
@@ -65,6 +65,125 @@ export async function getCharacter(name) {
 
 }
 
+export async function getMap(x, y) {
+    return await sendRequestToURL(`https://api.artifactsmmo.com/maps/${x}/${y}`, '', "GET");
+}
+
+const commonResourceCache = {
+    ash_tree: {
+        data: {
+            name: "Ash Tree",
+            code: "ash_tree",
+            skill: "woodcutting",
+            level: 1,
+            drops: [
+                {
+                    code: "ash_wood",
+                    rate: 1,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+                {
+                    code: "sap",
+                    rate: 10,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+            ],
+        },
+    },
+    copper_rocks: {
+        data: {
+            name: "Copper Rocks",
+            code: "copper_rocks",
+            skill: "mining",
+            level: 1,
+            drops: [
+                {
+                    code: "copper_ore",
+                    rate: 1,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+                {
+                    code: "topaz_stone",
+                    rate: 600,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+                {
+                    code: "topaz",
+                    rate: 5000,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+                {
+                    code: "emerald",
+                    rate: 5000,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+                {
+                    code: "emerald_stone",
+                    rate: 600,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+                {
+                    code: "ruby",
+                    rate: 5000,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+                {
+                    code: "ruby_stone",
+                    rate: 600,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+                {
+                    code: "sapphire",
+                    rate: 5000,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+                {
+                    code: "sapphire_stone",
+                    rate: 600,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+            ],
+        },
+    },
+    gudgeon_fishing_spot: {
+        data: {
+            name: "Gudgeon Fishing Spot",
+            code: "gudgeon_fishing_spot",
+            skill: "fishing",
+            level: 1,
+            drops: [
+                {
+                    code: "gudgeon",
+                    rate: 1,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+                {
+                    code: "algae",
+                    rate: 10,
+                    min_quantity: 1,
+                    max_quantity: 1,
+                },
+            ],
+        },
+    },
+};
+export async function getResource(code) {
+    if(commonResourceCache[code]) return commonResourceCache[code]
+    return await sendRequestToURL(`https://api.artifactsmmo.com/resources/${code}`, '', "GET");
+}
+
 export async function depositItem(character, item) {
     const body = { code: item.code, quantity: item.quantity };
     return await sendRequest(character, "/action/bank/deposit", "POST", JSON.stringify(body));
